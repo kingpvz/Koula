998ba5c7f8b514df2bc11ded62afe5ce0ad8cdb7
Stanley <stano5340@gmail.com>
2024-04-26 17:34:00 +0200

    HOTFIX at class Movable changed position to meet the new syntax

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from random import randint as rnd
 class Movable:
     def __init__(t, p, **s):
         t.id = p.g.create_oval(103,103,111,111, outline="#7777FF", width=2)
-        t.position = {"xl":100, "xr":110, "yt":110, "yb":100}
+        t.position = {"x":10, "y": 10}
         
     def move(t,p):
          x,y = rnd(-1,1), rnd(-1,1)
