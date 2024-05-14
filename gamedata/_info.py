c2e2a4a66729c2764976322db2bb2c7fd6346eff
Stanley <109061209+kingpvz@users.noreply.github.com>
2024-05-14 09:18:50 +0200

    Update _info.py to beta3.3

--- gamedata/_info.py
+++ gamedata/_info.py
@@ -1,4 +1,4 @@
-VERSION = "beta3.2"
+VERSION = "beta3.3"
 
 HELP = """Welcome to KOULA!
 (the best game ever for real)
@@ -11,7 +11,7 @@ R - Restart Level
 
 This is a quick tutorial to help you get started.
 Every level consists of many small balls on the screen. And the goal is simple: Get the amount of points you need to win the level.
-Every ball has a different purpose and acts differently too. If you wish to know what each ball does, press V in-game to open the WIKI!
+Every ball has a different purpose and acts differently too. If you wish to know what each ball does, use the Wiki!
 
 Levels will always give you at least the amount of points you need. Basically try not to lose any. Simple as that!
 """
